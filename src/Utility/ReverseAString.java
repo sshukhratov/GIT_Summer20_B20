53045a407f3eed026aed93db1ae7a49fe737e8de
sshukhratov <sshukhratov@gmail.com>
2020-08-05 19:31:43 -0500

    ReverseAString.java

---
+++ src/Utility/ReverseAString.java
@@ -0,0 +1,7 @@
+package Utility;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        // Reversed
+    }
+}
--- src/Utility/ZombiAttack.java
+++ src/Utility/ZombiAttack.java
@@ -8,6 +8,8 @@ public class ZombiAttack {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
+
         int[] inhabitants = new int[8];
         for(int i=0; i<inhabitants.length; i++) {
             inhabitants[i] = input.nextInt();
